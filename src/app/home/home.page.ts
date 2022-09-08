ad86cdc59a4779f548fd28e1b9f4ea9df6dc7b1c
kimberly McIntyre <mcintyre@ionic.io>
2022-09-08 14:30:05 -0400

    Example 1: Get Configurations

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Deploy } from 'cordova-plugin-ionic/dist/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  appid: string = 'TBD';
+  constructor(private _deploy: Deploy) {}
 
-  constructor() {}
-
+  async getUpdateConfig() {
+    const info = (await this._deploy.getConfiguration()).appId;
+    this.appid = info;
+  } 
 }
