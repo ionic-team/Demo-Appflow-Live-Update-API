69910b4eb7a94ecbc9effc59b43a7ff0338b77cc
kimberly McIntyre <mcintyre@ionic.io>
2022-09-08 16:49:24 -0400

    Example 1: Reload App With Manaul Update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,29 @@ import { Deploy } from 'cordova-plugin-ionic/dist/ngx';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  updateStatus: string = 'Update not started';
 
   constructor(private _deploy: Deploy) {}
+  
+  async performManualUpdate() {
+    this.updateStatus = 'Checking for Update';
+    const update = await this._deploy.checkForUpdate()
+    if (update.available){
+      this.updateStatus = 'Update found. Downloading update';
+      await this._deploy.downloadUpdate((progress) => {
+        console.log(progress);
+      })
+      this.updateStatus = 'Update downloaded. Extracting update';
+      await this._deploy.extractUpdate((progress) => {
+        console.log(progress);
+      })
+      console.log('Reloading app');
+      this.updateStatus = 'Update extracted. Reloading app';
+      await this._deploy.reloadApp();
+    } else {
+      console.log('No update available');
+      this.updateStatus = 'No update available';
+    }
+   }
+ 
 }
